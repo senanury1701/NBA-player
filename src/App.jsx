5a4578987edf18611f88b6a1388cc7b5d51f0352
senanury1701 <senanury1701@gmail.com>
2024-01-19 16:10:50 +0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './pages'
+import './App.css'
+import Header from './components/Header'
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <div>
+            haha
+        </div>
+    )
+}
+export default Home
