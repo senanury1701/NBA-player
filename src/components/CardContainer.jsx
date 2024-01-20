718bf118a704d55cbc916504c6db2f18466c9248
senanury1701 <senanury1701@gmail.com>
2024-01-20 17:48:04 +0300

    .

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,27 +1,33 @@
-import './card.css'
+import './card.css';
+import  { useState } from 'react';
 
 const Card = (data) => {
-    if (!data) {
-        console.error('Data is undefined')
-        return null
-    }
+    const [show, setShow] = useState(false);
+
+    const visibility = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
     return (
-        <div className='card'>
-            <div className='card-front'>
-                <img src={data.img} alt="" />
-                <div className='overlay'>
-                    <div className='text'>{data.name}</div>   
+        <div className='card' onClick={visibility}>
+            {show ? (
+                <div className='card-back'>
+                    <ul>
+                        {data.statistics.map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
                 </div>
-                         
-            </div>
-            <div className='card-back'>
-                <ul>
-                    {data.statistics.map((item) => <li key={item}>{item}</li> )}
-                </ul>
-            </div>
-
-
+            ) : (
+                <div className='card-front'>
+                    <img src={data.img} alt="" />
+                    <div className='overlay'>
+                        <div className='text'>{data.name}</div>
+                    </div>
+                </div>
+            )}
         </div>
-    )
-}
-export default Card
+    );
+};
+
+export default Card;
